ed52ccaa4b5b8a2fecfc6ae8b2fa6bc48cc69012
Oscar Ayala <fabianayala675@gmail.com>
2025-11-02 15:24:40 -0300

    Generar y verificar JWT

--- backend/src/utils/hash.ts
+++ backend/src/utils/hash.ts
@@ -1,10 +1,12 @@
 import bcrypt from 'bcryptjs'
 
+//Hash password
 export const hashPassword =async (password: string) => {
     const salt = await bcrypt.genSalt(10)
     return bcrypt.hash(password, salt)
 }
 
+//Compare password
 export const comparePassword = async ( password: string, hashed: string) => {
     const result = await bcrypt.compare(password, hashed)
     return result
